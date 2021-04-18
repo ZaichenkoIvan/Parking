3b195f86fe9b8fa122f484b1c6e55911508923fc
Zaichenko, Ivan <Ivan.Zaichenko@McKesson.com>
2021-04-18 18:51:37 +0300

    add methods for find index value

--- parking/src/main/java/com/example/parking/repository/CouponRepository.java
+++ parking/src/main/java/com/example/parking/repository/CouponRepository.java
@@ -1,5 +1,8 @@
 package com.example.parking.repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,5 @@ import com.example.parking.entity.CouponEntity;
 
 @Repository
 public interface CouponRepository extends JpaRepository<CouponEntity, Long> {
+	Optional<CouponEntity> findByStartDateAndEndDate(LocalDate startDate, LocalDate endDate);
 }
--- parking/src/main/java/com/example/parking/repository/ParkingRepository.java
+++ parking/src/main/java/com/example/parking/repository/ParkingRepository.java
@@ -1,5 +1,7 @@
 package com.example.parking.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.example.parking.entity.ParkingEntity;
 
 @Repository
 public interface ParkingRepository extends JpaRepository<ParkingEntity, Long> {
+	Optional<ParkingEntity> findByName(String name);
 }
